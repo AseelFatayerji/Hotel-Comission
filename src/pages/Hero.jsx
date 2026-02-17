09cf414e35cb7ba4dfef74aed7e6de416f89447e
AseelFatayerji <161306291+AseelFatayerji@users.noreply.github.com>
2026-02-17 03:49:16 +0200

    addedquick booking

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
 
 function Hero() {
   return (
-    <div id="Home" className='p-10 justify-center w-screen h-screen background'>
-      Hero
+    <div
+      id="Home"
+      className="flex p-10 justify-center items-center w-screen h-screen background"
+    >
+      <div className="bg-white rounded-lg px-10 py-5 flex justify-evenly gap-10">
+        <div>
+          Check Out <br /> <input type="date" alt="check out date" />
+        </div>
+        <div>
+          Check In
+          <br /> <input type="date" alt="check in date" />
+        </div>
+        <div>
+          Room Type
+          <br />
+          <select>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+          </select>
+        </div>
+        <div>
+          Guests <br />
+          <input type="number" alt="number of guests" defaultValue={0} />
+        </div>
+        <button>Book Now</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
