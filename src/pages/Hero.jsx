a10ba740a6421229539ace11a6cea7ac0457db1e
AseelFatayerji <161306291+AseelFatayerji@users.noreply.github.com>
2026-02-17 03:35:16 +0200

    added temp image

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Hero() {
   return (
-    <div id="Home" className='p-10 justify-center w-screen h-screen'>
+    <div id="Home" className='p-10 justify-center w-screen h-screen background'>
       Hero
     </div>
   )
