db3aeac8fd8723cbc7aa5e3c327bca8d023f4cd3
AseelFatayerji <161306291+AseelFatayerji@users.noreply.github.com>
2026-02-17 21:06:02 +0200

    adjusted card design

--- src/components/Room_Card.jsx
+++ src/components/Room_Card.jsx
@@ -1,3 +1,6 @@
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 function RoomCard({ img, name }) {
   return (
     <div
@@ -5,9 +8,9 @@ function RoomCard({ img, name }) {
       style={{ backgroundImage: `url(${img})`, backgroundSize: "cover" }}
       alt={name}
     >
-      <div className="text-xl absolute inset-x-0 bottom-0 space-y-2 bg-linear-to-t rounded-b-2xl  from-black to-transparent h-fit p-5">
-        <div className="font-bold text-white">{name}</div>
-        <button className="self-end">View Room</button>
+      <div className="text-xl absolute flex justify-between inset-x-0 bottom-0 bg-linear-to-t rounded-b-2xl  from-black to-transparent h-fit p-5">
+        <div className="font-bold bg-white rounded-full px-5 py-1 text-center justify-center">{name}</div>
+        <button className="text-center"><FontAwesomeIcon icon={faArrowUp} className="rotate-45"/></button>
       </div>
     </div>
   );
