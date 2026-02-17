60090bf96b746574f0597118a107da87a7c07112
AseelFatayerji <161306291+AseelFatayerji@users.noreply.github.com>
2026-02-17 02:22:59 +0200

    made navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import { HashLink as NavLink } from "react-router-hash-link";
 
 function Navbar() {
   return (
-    <div className='flex justify-between w-screen px-4 py-3 select-none'>
-      <div className=''>Logo.</div>
-      <div className='bg-lime-300/40'>Navbar</div>
-      <div className='text-transparent'>Menue</div>
-    </div>
-  )
+    <nav className="flex justify-between w-screen px-4 py-3 select-none fixed top-0 left-0">
+      <div className="">Logo.</div>
+      <div className="bg-[#87d551]/40 px-4 py-2 text-lg rounded-lg flex justify-evenly gap-10 font-bold">
+        <NavLink to="#Hero">Home</NavLink>
+        <NavLink to="#Rooms">Rooms</NavLink>
+        <NavLink to="#Contact">Contact</NavLink>
+      </div>
+      <div className="text-transparent">Menue</div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
