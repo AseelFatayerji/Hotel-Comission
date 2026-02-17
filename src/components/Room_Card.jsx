39abfc4cf6aa35c586335b0aa1023f394249aed4
AseelFatayerji <161306291+AseelFatayerji@users.noreply.github.com>
2026-02-17 21:47:47 +0200

    added section title

--- src/components/Room_Card.jsx
+++ src/components/Room_Card.jsx
@@ -11,12 +11,12 @@ function RoomCard({ img, name }) {
 
   return (
     <div
-      className="rounded-2xl h-92 w-72 relative align-bottomp p-5"
+      className="rounded-2xl h-92 w-72 relative border-2 border-[#87d551] align-bottomp p-5 shadow-[10px_7px_12px_2px_rgba(0,0,0,0.2)] hover:-mt-5 hover:ease-in-out "
       style={{ backgroundImage: `url(${img})`, backgroundSize: "cover" }}
       alt={name}
     >
       <div className="text-xl absolute flex justify-between inset-x-0 bottom-0 bg-linear-to-t rounded-b-2xl  from-black to-transparent h-fit p-5">
-        <div className="font-bold bg-white rounded-full px-5 py-1 text-center justify-center">
+        <div className="font-medium bg-white rounded-full px-5 py-1 text-center justify-center">
           {name}
         </div>
         <button className="text-center" onClick={handleClick}>
--- src/pages/Rooms.jsx
+++ src/pages/Rooms.jsx
@@ -9,14 +9,22 @@ function Room() {
     { id: 4, img: "src/assets/temp-room.jpg", name: "Room Name4" },
   ];
   return (
-    <div id="Rooms" className="w-screen h-fit bg-white rounded-t-3xl flex justify-evenly gap-10 -mt-10 p-10">
-      {rooms.map((room) => {
-        return (
-          <div key={room.id}>
-            <RoomCard img={room.img} name={room.name} />
-          </div>
-        );
-      })}
+    <div
+      id="Rooms"
+      className="w-screen h-fit bg-white rounded-t-[3rem] space-y-5 -mt-15 p-5"
+    >
+      <label className="flex text-6xl w-full gap-5 justify-center p-5">
+        Random <l className="text-[#87d551] underline">Insprational</l> Quote
+      </label>
+      <div className="flex justify-evenly gap-10">
+        {rooms.map((room) => {
+          return (
+            <div key={room.id}>
+              <RoomCard img={room.img} name={room.name} />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
