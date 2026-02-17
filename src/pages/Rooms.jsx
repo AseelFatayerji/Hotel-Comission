214da2d2ddf8bbccfe1ae1ee61d9ddaab7cc843e
AseelFatayerji <161306291+AseelFatayerji@users.noreply.github.com>
2026-02-17 20:23:01 +0200

    created room section

--- src/pages/Rooms.jsx
+++ src/pages/Rooms.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import RoomCard from "../components/Room_Card";
 
 function Room() {
+  const rooms = [
+    { id: 1, img: "src/assets/temp-room.jpg", name: "Room Name" },
+    { id: 2, img: "src/assets/temp-room.jpg", name: "Room Name" },
+    { id: 3, img: "src/assets/temp-room.jpg", name: "Room Name" },
+  ];
   return (
-    <div id="Rooms">
-      Room
+    <div id="Rooms" className="w-screen h-fit rounded-t-3xl">
+      {rooms.map((room) => {
+        return (
+          <div key={room.id}>
+            <RoomCard img={room.img} name={room.name} />
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
 
-export default Room
+export default Room;
