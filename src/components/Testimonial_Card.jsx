a99d4a61ce4079cfd7f7cb14f1f437256c62616b
AseelFatayerji <161306291+AseelFatayerji@users.noreply.github.com>
2026-02-19 01:52:49 +0200

    fixed stars

--- src/components/Testimonial_Card.jsx
+++ src/components/Testimonial_Card.jsx
@@ -1,50 +1,37 @@
-import {
-  faStar,
-  faStarAndCrescent,
-  faStarHalfStroke,
-} from "@fortawesome/free-solid-svg-icons";
+import { faStar, faStarHalfStroke } from "@fortawesome/free-solid-svg-icons";
+import { faStar as Empty } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useEffect } from "react";
 
 function Testimonial_Card({ rating, name, review, img }) {
   const stars = [];
-  useEffect(() => {
-    if (rating < 5) {
-      for (let index = 1; index <= rating; index++) {
-        stars.concat(faStar);
-      }
-      if (rating % 10 == 5) {
-        stars.concat(faStarHalfStroke);
-        for (let index = rating; index <= 5; index++) {
-          stars.concat(faStarAndCrescent);
-        }
-      } else {
-        for (let index = rating; index <= 5; index++) {
-          stars.concat(faStarAndCrescent);
-        }
-      }
-    } else {
-      for (let index = 1; index <= rating; index++) {
-        stars.concat(faStar);
-      }
-    }
-  }, [stars]);
+
+  const fullStars = Math.floor(rating);
+  const hasHalf = rating % 1 !== 0;
+  const emptyStars = 5 - fullStars - (hasHalf ? 1 : 0);
+
+  for (let i = 0; i < fullStars; i++) {
+    stars.push(faStar);
+  }
+  if (hasHalf) stars.push(faStarHalfStroke);
+  for (let i = 0; i < emptyStars; i++) {
+    stars.push(Empty);
+  }
+
   return (
-    <div className="flex flex-col p-5 border-2 border-[#B4B4B4] bg-neutral-100 rounded-lg shrink-0 w-[20rem]">
-      <div className="text-[#87d551]">
+    <div className="flex flex-col py-2 px-5 border-2 border-[#B4B4B4] bg-neutral-100 rounded-2xl shrink-0 w-[20rem]">
+      <div className="text-[#87d551] gap-2 flex py-3 text-xl">
         {stars.map((star, index) => {
           return <FontAwesomeIcon icon={star} key={index} />;
         })}
       </div>
-      <div className="text-md ">{review}</div>
+      <div className="text-sm">{review}</div>
       <div className="w-full border-t  border-[#B4B4B4] my-4"></div>
-      <div className="flex gap-5 text-lg items-center">
+      <div className="flex gap-5 text-lg items-center py-2">
         <img
           src={img}
           className="rounded-full border-4 border-[#87d551] h-15 w-15"
         />
         <div className="h-fit">{name}</div>
-        
       </div>
     </div>
   );
