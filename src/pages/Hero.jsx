cb20d1435800cea84dae3b60984334798069c792
AseelFatayerji <161306291+AseelFatayerji@users.noreply.github.com>
2026-02-22 23:46:03 +0200

    adjusted button css

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
 
 function Hero() {
+  const [startDate, setStartDate] = useState(new Date());
   return (
     <div id="Hero" className="backgrounds ">
       <div className="bg-black/30 w-screen h-screen justify-center items-center p-10 flex flex-col space-y-10">
@@ -10,7 +12,12 @@ function Hero() {
         <div className="bg-white rounded-2xl px-15 py-10 flex justify-evenly gap-10 w-fit text-left Poppins text-xl">
           <div>
             Check Out <br />
-            <input type="date" className="text-sm" alt="check out date" />
+            <div className="text-sm">
+              <DatePicker
+                selected={startDate}
+                onChange={(date) => setStartDate(date)}
+              />
+            </div>
           </div>
           <div>
             Check In
@@ -35,7 +42,7 @@ function Hero() {
               className="w-20 text-sm"
             />
           </div>
-          <button className="rounded-lg border-2 py-1.5 px-6 border-[#87d551]">
+          <button className="rounded-lg border-2 py-1.5 px-6 border-[#87d551] custom-button">
             Book Now
           </button>
         </div>
