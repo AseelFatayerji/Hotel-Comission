e7d521ecc91dde7f8cc721abbf16bdd724aeecc9
AseelFatayerji <161306291+AseelFatayerji@users.noreply.github.com>
2026-02-26 00:38:02 +0200

    made the hero section responsive

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,30 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
 import DatePicker from "react-datepicker";
 
 function Hero() {
-  const [startDate, setStartDate] = useState(new Date());
   return (
     <div id="Hero" className="backgrounds ">
-      <div className="bg-black/30 w-screen h-screen justify-center items-center p-10 flex flex-col space-y-10">
-        <div className="text-white text-8xl font-bold text-center">
+      <div className="bg-black/30 w-screen h-fit py-20 px-10 flex flex-col items-center md:space-y-10 md:justify-center md:h-screen md:py-10">
+        <div className="text-white text-5xl font-bold text-center text-pretty pt-5 align-text-top md:text-8xl md:align-middle ">
           Land Of Peace <br /> LOP Inn
         </div>
-        <div className="bg-white rounded-2xl px-15 py-10 flex justify-evenly gap-10 w-fit text-left Poppins text-xl">
+        <div className="bg-white rounded-2xl flex p-7 justify-evenly gap-10 w-fit text-left Poppins text-xl scale-55 md:scale-100 md:px-15 md:py-10 ">
           <div>
             Check Out <br />
-            <div className="text-sm">
-              <DatePicker
-                selected={startDate}
-                onChange={(date) => setStartDate(date)}
-              />
-            </div>
+            <input type="date" className="text-sm" alt="check out date" />
           </div>
           <div>
             Check In
             <br /> <input type="date" className="text-sm" alt="check in date" />
           </div>
           <div>
-            Room Type
+            Room
             <br />
             <select className="text-sm">
               <option>1</option>
@@ -42,7 +36,7 @@ function Hero() {
               className="w-20 text-sm"
             />
           </div>
-          <button className="rounded-lg border-2 py-1.5 px-6 border-[#87d551] custom-button">
+          <button className="rounded-lg border-2 py-1.5 px-6 border-[#87d551]">
             Book Now
           </button>
         </div>
