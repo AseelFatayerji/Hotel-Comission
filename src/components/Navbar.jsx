9d8236e3b383b0a366291ff13fd49b10bb270cb3
AseelFatayerji <161306291+AseelFatayerji@users.noreply.github.com>
2026-03-05 02:52:41 +0200

    made navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,9 @@ function Navbar() {
   return (
     <nav className="flex justify-between items-center w-screen px-4 py-3 select-none fixed top-0 left-0 z-10">
       <div className="">
-        <img src={logo} alt="Land Of Peace" className="w-20 h-20" />
+        <img src={logo} alt="Land Of Peace" className="w-15 h-15 md:w-20 md:h-20" />
       </div>
-      <div className="bg-[#87d551]/60 backdrop-blur-sm px-4 h-fit py-2 text-lg rounded-lg flex justify-evenly gap-10 font-bold">
+      <div className="bg-[#87d551]/60 backdrop-blur-sm  h-fit px-1 py-2 text-sm rounded-lg flex justify-evenly gap-1 font-bold md:text-lg md:px-4 md:gap-10">
         <NavLink to="/">Home</NavLink>
         <NavLink to="/#Rooms">Rooms</NavLink>
         <NavLink to="/#Contact">Contact</NavLink>
