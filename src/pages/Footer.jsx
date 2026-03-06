56506cce09ccb82bd86c17960475a07dc79606b7
AseelFatayerji <161306291+AseelFatayerji@users.noreply.github.com>
2026-03-06 18:17:28 +0200

    adjusted letter sizes

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -11,13 +11,13 @@ import logo from "../assets/logo.png";
 function Footer() {
   const isMobile = useMediaQuery({ query: "(max-width: 810px)" });
   return (
-    <footer className="flex flex-col w-screen px-4 py-2 rounded-t-[20px] bg-[#87d551] text-neutral-200 Poppins md:px-14">
+    <footer className="flex flex-col w-screen px-4 py-2 rounded-t-[20px] bg-[#87d551] text-neutral-200 Poppins md:px-10">
       <div className="flex flex-row pt-3">
         <div className="flex justify-start h-20 w-20 pr-3 md:w-[35%] ">
-          <img alt="Land Of Peace Logo" src={logo} width="80" />
+          <img alt="Land Of Peace Logo" src={logo} className="h-15 w-15 md:h-20 md:w-20" />
         </div>
-        <div className="flex flex-row w-[65%] justify-end gap-16 text-nowrap text-sm md:text-lg">
-          <div className="grid grid-cols-3 gap-16 md:gap-12">
+        <div className="flex w-[65%] justify-end text-nowrap text-sm md:text-lg">
+          <div className="grid grid-cols-3 gap-16 md:gap-8">
             <div className="flex flex-col ">
               <div className="font-bold uppercase text-white md:pb-2">
                 Land Of {isMobile && <br />} Peace
@@ -47,8 +47,8 @@ function Footer() {
           </div>
         </div>
       </div>
-      <div className="w-full border-t  border-gray-500 my-8" />
-      <div className="text-center text-white">
+      <div className="w-full border-t  border-gray-500 my-4 md:my-8" />
+      <div className="text-center text-sm text-white">
         © 2026 Land Of Peace - All rights reserved.
       </div>
     </footer>
