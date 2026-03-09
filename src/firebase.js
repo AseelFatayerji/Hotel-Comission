6238ba70c61d5df0f7e9ac904ad3bcdcc85703b8
AseelFatayerji <161306291+AseelFatayerji@users.noreply.github.com>
2026-03-09 03:30:04 +0200

    added firestore

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: "lopinn-37bc3.firebaseapp.com",
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: "lopinn-37bc3.firebasestorage.app",
+  messagingSenderId: "120785346542",
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: "G-7WBBCX1X4G"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
