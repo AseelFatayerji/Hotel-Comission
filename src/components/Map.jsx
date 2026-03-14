70d227c2a2f195593030ed8f71c3d4b47920a24e
AseelFatayerji <161306291+AseelFatayerji@users.noreply.github.com>
2026-03-14 18:59:09 +0200

    adjusted marker location and zoom

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,14 +1,21 @@
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 function Map() {
-  const location = { lat: 9.976987281724702, lng: -84.16001289140435 };
+  const location = { lat: 9.976795, lng: -84.160072 };
+
+  const handleCopy = async () => {
+    console.log("copied");
+    navigator.clipboard.writeText("https://maps.app.goo.gl/ng41PCsG1myF1pFF9");
+  };
   return (
     <LoadScript googleMapsApiKey={import.meta.env.VITE_MAP_KEY}>
       <GoogleMap
-        mapContainerClassName="rounded-lg w-full h-[30rem]"
+        mapContainerClassName="rounded-2xl w-full h-[30rem]"
         center={location}
-        zoom={15}
-      />
+        zoom={20}
+      >
+        <Marker position={location} onClick={handleCopy} text="LOP Inn"/>
+      </GoogleMap>
     </LoadScript>
   );
 }
