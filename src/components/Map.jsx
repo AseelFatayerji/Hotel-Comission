1fb4add5f7268540e1429e6a40d25f2bccc0afba
AseelFatayerji <161306291+AseelFatayerji@users.noreply.github.com>
2026-03-19 00:50:49 +0200

    updated google maps

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,22 +1,33 @@
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import { useRef } from "react";
+import Loadings from "./Loadings";
+
+const libraries = ["marker"];
 
 function Map() {
   const location = { lat: 9.976795, lng: -84.160072 };
+  const mapRef = useRef(null);
 
-  const handleCopy = async () => {
-    console.log("copied");
-    navigator.clipboard.writeText("https://maps.app.goo.gl/ng41PCsG1myF1pFF9");
-  };
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: import.meta.env.VITE_MAP_KEY,
+    libraries,
+    version: "weekly"
+  });
+
+  if (!isLoaded) return <Loadings />;
+
   return (
-    <LoadScript googleMapsApiKey={import.meta.env.VITE_MAP_KEY}>
-      <GoogleMap
-        mapContainerClassName="rounded-2xl w-full h-[30rem]"
-        center={location}
-        zoom={20}
-      >
-        <Marker position={location} onClick={handleCopy} text="LOP Inn"/>
-      </GoogleMap>
-    </LoadScript>
+    <GoogleMap
+      mapContainerClassName="rounded-2xl w-full h-[30rem]"
+      center={location}
+      zoom={20}
+      onLoad={(map) => {
+        mapRef.current = map;
+      }}
+      options={{
+        mapId: import.meta.env.VITE_MAP_ID 
+      }}
+    />
   );
 }
 
